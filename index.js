2538ca6edd911baca7a32a6c3dab18c76070b16e
Juan <ssjmatos@gmail.com>
2021-12-06 13:09:21 -0500

    nodes lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "<h1>Jessica is the champion!</h1>";
